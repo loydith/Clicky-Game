bb48d52bbebe2299692cde96ec4220e65d7a55f9
Loydith Valdivia <loydith@gmail.com>
2019-07-20 15:56:22 -0500

    create the files and react

---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FadeIn from '../transitions/fade-in';
+
+const Header = () => { return(
+    <header className="App-header">
+        <FadeIn in={true} duration={600} length={'30px'} direction={'bottom'}>
+            <h2>click on an image to earn points, but don't click on any more than once!</h2>
+        </FadeIn>
+        <FadeIn in={true} duration={600} length={'50px'} direction={'bottom'} >
+            <h1>Dogs Clicky Game</h1>
+        </FadeIn>
+    </header>
+)};
+
+export default Header;
